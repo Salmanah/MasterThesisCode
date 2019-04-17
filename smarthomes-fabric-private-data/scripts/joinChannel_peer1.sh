e7e14ff9ec813b006a5cc6d66fdebaf3594c5bcb
salmanah <salardahm@gmail.com>
2019-04-17 21:49:15 +0200

    Working on smarthomes-fabric-private-data, createChannel works but peers cannot join. WEIRD

--- smarthomes-fabric-private-data/scripts/joinChannel_peer1.sh
+++ smarthomes-fabric-private-data/scripts/joinChannel_peer1.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-docker cp mychannel.block peer0.org2.example.com:/mychannel.block
 
-docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org2.example.com/msp" peer0.org2.example.com peer channel join -b ../mychannel.block
-
-rm mychannel.block
+docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" -e "CORE_PEER_ADDRESS=peer0.org2.example.com:8051" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" peer0.org2.example.com peer channel join -b ../mychannel.block
