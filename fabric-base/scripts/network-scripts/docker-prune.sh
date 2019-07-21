6fa40aa0bea76870527e2a98177cb34fccc8ca1e
salmanah <salardahm@gmail.com>
2019-07-21 16:57:20 +0200

    Pushing so i can pull new crypto-material for fabric-base

---
+++ fabric-base/scripts/network-scripts/docker-prune.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Killing all docker containers and images"
+#Open 4 terminal tabs
+	tmux send -t CA.0  "docker system prune -a" ENTER ##Orderer
+	tmux send -t CA.1  "docker system prune -a" ENTER ##Peer4
+	tmux send -t CA.2  "docker system prune -a" ENTER ##Peer0
+	tmux send -t CA.3  "docker system prune -a" ENTER ##Peer2
+	tmux send -t CA.4  "docker system prune -a" ENTER #Peer1
+	tmux send -t CA.5  "docker system prune -a" ENTER #Peer3
+
+	
+
+
+
