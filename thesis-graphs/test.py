33d7351671da475a9feb95c52bfd4432e4072954
salmanah <salardahm@gmail.com>
2019-08-01 18:26:59 +0200

    Finished plotting latency

--- thesis-graphs/test.py
+++ thesis-graphs/test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-p = (1013,1513,3038)
+p = (100,250,500)
 y_pos = np.arange(len(p))
 #FABRIC_BASE = RED
 #FABRIC-SOLO = BLUE
@@ -13,9 +13,9 @@ y_pos = np.arange(len(p))
 fig, axs = plt.subplots(4, 3, constrained_layout=True)
 
 fabricBase =axs[0][0].barh(y_pos+0.1, [2.70,2.93,2.73],height=0.2, color="red")
-fabricSolo =axs[0][0].barh(y_pos-0.1, [2.69,2.39,1.78],height=0.2, color='blue')
+fabricSolo =axs[0][0].barh(y_pos-0.1, [2.71,2.82,2.62],height=0.2, color='blue')
 fabricTLS =axs[0][0].barh(y_pos-0.3, [2.86,2.91,2.69],height=0.2, color='green')
-fabricPrivate =axs[0][0].barh(y_pos+0.3, [24.88,34.26,20.41],height=0.2, color='cyan')
+fabricPrivate =axs[0][0].barh(y_pos+0.3, [13.10,29.53,34.68],height=0.2, color='cyan')
 
 axs[0][0].set_title('5 clients 25tps')
 axs[0][0].set_ylabel('Payload in bytes')
@@ -31,9 +31,9 @@ axs[0][0].invert_yaxis()
 
 axs[1][0].set_title('5 clients 50tps')
 axs[1][0].barh(y_pos+0.1, [1.97,2.1,2.11],height=0.2, color="red")
-axs[1][0].barh(y_pos-0.1, [2.32,2.21,1.62],height=0.2, color='blue')
+axs[1][0].barh(y_pos-0.1, [2.01,1.98,1.84],height=0.2, color='blue')
 axs[1][0].barh(y_pos-0.3, [1.89,1.84,1.93],height=0.2, color='green')
-axs[1][0].barh(y_pos+0.3, [35.18,34.08,33.04],height=0.2, color='cyan')
+axs[1][0].barh(y_pos+0.3, [24.57,21.91,32.00],height=0.2, color='cyan')
 
 axs[1][0].set_ylabel('Payload in bytes')
 axs[1][0].set_xlabel('Average latency (seconds)')
@@ -48,9 +48,9 @@ axs[1][0].invert_yaxis()
 
 axs[2][0].set_title('5 clients 100tps')
 axs[2][0].barh(y_pos+0.1, [3.62,3.19,4.38],height=0.2, color="red")
-axs[2][0].barh(y_pos-0.1, [7.65,5.61,7.60],height=0.2, color='blue')
+axs[2][0].barh(y_pos-0.1, [4.48,4.83,2.70],height=0.2, color='blue')
 axs[2][0].barh(y_pos-0.3, [2.93,3.46,2.42],height=0.2, color='green')
-axs[2][0].barh(y_pos+0.3, [34.35,36.16,36.93],height=0.2, color='cyan')
+axs[2][0].barh(y_pos+0.3, [27.53,26.93,26.24],height=0.2, color='cyan')
 
 axs[2][0].set_ylabel('Payload in bytes')
 axs[2][0].set_xlabel('Average latency (seconds)')
@@ -65,9 +65,9 @@ axs[2][0].invert_yaxis()
 
 axs[3][0].set_title('5 clients 1000tps')
 axs[3][0].barh(y_pos+0.1, [8.99,8.21,10.06],height=0.2, color="red")
-axs[3][0].barh(y_pos-0.1, [10.63,12.20,13.88],height=0.2, color='blue')
+axs[3][0].barh(y_pos-0.1, [7.71,8.87,8.11],height=0.2, color='blue')
 axs[3][0].barh(y_pos-0.3, [9.52,8.54,8.97],height=0.2, color='green')
-axs[3][0].barh(y_pos+0.3, [40.47,36.80,37.93],height=0.2, color='cyan')
+axs[3][0].barh(y_pos+0.3, [34.62,29.53,34.68],height=0.2, color='cyan')
 
 axs[3][0].set_ylabel('Payload in bytes')
 axs[3][0].set_xlabel('Average latency (seconds)')
@@ -82,9 +82,9 @@ axs[3][0].invert_yaxis()
 
 axs[0][1].set_title('10 clients 25tps')
 axs[0][1].barh(y_pos+0.1, [2.75,2.79,2.76],height=0.2, color="red")
-axs[0][1].barh(y_pos-0.1, [2.68,2.40,1.79],height=0.2, color='blue')
+axs[0][1].barh(y_pos-0.1, [2.71,2.87,2.62],height=0.2, color='blue')
 axs[0][1].barh(y_pos-0.3, [2.73,2.84,2.66],height=0.2, color='green')
-axs[0][1].barh(y_pos+0.3, [22.34,23.74,19.55],height=0.2, color='cyan')
+axs[0][1].barh(y_pos+0.3, [21.10,8.21,9.89],height=0.2, color='cyan')
 
 axs[0][1].set_ylabel('Payload in bytes')
 axs[0][1].set_xlabel('Average latency (seconds)')
@@ -99,9 +99,9 @@ axs[0][1].invert_yaxis()
 
 axs[1][1].set_title('10 clients 50tps')
 axs[1][1].barh(y_pos+0.1, [1.93,1.99,2.16],height=0.2, color="red")
-axs[1][1].barh(y_pos-0.1, [2.00,2.01,1.71],height=0.2, color='blue')
+axs[1][1].barh(y_pos-0.1, [1.84,1.94,1.75],height=0.2, color='blue')
 axs[1][1].barh(y_pos-0.3, [1.98,1.98,1.82],height=0.2, color='green')
-axs[1][1].barh(y_pos+0.3, [36.12,34.06,33.50],height=0.2, color='cyan')
+axs[1][1].barh(y_pos+0.3, [22.56,20.62,24.41],height=0.2, color='cyan')
 
 axs[1][1].set_ylabel('Payload in bytes')
 axs[1][1].set_xlabel('Average latency (seconds)')
@@ -116,9 +116,9 @@ axs[1][1].invert_yaxis()
 
 axs[2][1].set_title('10 clients 100tps')
 axs[2][1].barh(y_pos+0.1, [1.83,2.49,3.43],height=0.2, color="red")
-axs[2][1].barh(y_pos-0.1, [3.53,5.68,6.30],height=0.2, color='blue')
+axs[2][1].barh(y_pos-0.1, [2.18,1.96,1.41],height=0.2, color='blue')
 axs[2][1].barh(y_pos-0.3, [2.38,2.26,3.42],height=0.2, color='green')
-axs[2][1].barh(y_pos+0.3, [34.04,35.95,34.87],height=0.2, color='cyan')
+axs[2][1].barh(y_pos+0.3, [31.12,26.57,25.81],height=0.2, color='cyan')
 
 axs[2][1].set_ylabel('Payload in bytes')
 axs[2][1].set_xlabel('Average latency (seconds)')
@@ -132,9 +132,9 @@ axs[2][1].invert_yaxis()
 
 axs[3][1].set_title('10 clients 1000tps')
 axs[3][1].barh(y_pos+0.1, [7.66,9.70,7.37],height=0.2, color="red")
-axs[3][1].barh(y_pos-0.1, [9.42,13.11,12.80],height=0.2, color='blue')
+axs[3][1].barh(y_pos-0.1, [7.05,6.31,4.63],height=0.2, color='blue')
 axs[3][1].barh(y_pos-0.3, [8.97,7.92,8.08],height=0.2, color='green')
-axs[3][1].barh(y_pos+0.3, [39.11,41.77,38.04],height=0.2, color='cyan')
+axs[3][1].barh(y_pos+0.3, [32.78,29.67,28.92],height=0.2, color='cyan')
 
 axs[3][1].set_ylabel('Payload in bytes')
 axs[3][1].set_xlabel('Average latency (seconds)')
@@ -149,9 +149,9 @@ axs[3][1].invert_yaxis()
 
 axs[0][2].set_title('20 clients 25tps')
 axs[0][2].barh(y_pos+0.1, [2.70,2.98,2.75],height=0.2, color="red")
-axs[0][2].barh(y_pos-0.1, [2.60,2.34,1.77],height=0.2, color='blue')
+axs[0][2].barh(y_pos-0.1, [2.44,2.79,2.66],height=0.2, color='blue')
 axs[0][2].barh(y_pos-0.3, [2.72,2.88,2.63],height=0.2, color='green')
-axs[0][2].barh(y_pos+0.3, [21.74,20.72,21.08],height=0.2, color='cyan')
+axs[0][2].barh(y_pos+0.3, [16.87,15.85,12.49],height=0.2, color='cyan')
 
 axs[0][2].set_ylabel('Payload in bytes')
 axs[0][2].set_xlabel('Average latency (seconds)')
@@ -167,9 +167,9 @@ axs[0][2].invert_yaxis()
 
 axs[1][2].set_title('20 clients 50tps')
 axs[1][2].barh(y_pos+0.1, [1.93,2.17,2.05],height=0.2, color="red")
-axs[1][2].barh(y_pos-0.1, [2.53,1.86,1.75],height=0.2, color='blue')
+axs[1][2].barh(y_pos-0.1, [1.72,1.92,1.83],height=0.2, color='blue')
 axs[1][2].barh(y_pos-0.3, [1.98,2.42,1.82],height=0.2, color='green')
-axs[1][2].barh(y_pos+0.3, [34.07,32.38,34.50],height=0.2, color='cyan')
+axs[1][2].barh(y_pos+0.3, [25.26,25.68,24.38],height=0.2, color='cyan')
 
 axs[1][2].set_ylabel('Payload in bytes')
 axs[1][2].set_xlabel('Average latency (seconds)')
@@ -184,9 +184,9 @@ axs[1][2].invert_yaxis()
 
 axs[2][2].set_title('20 clients 100tps')
 axs[2][2].barh(y_pos+0.1, [3.39,3.37,1.83],height=0.2, color="red")
-axs[2][2].barh(y_pos-0.1, [5.68,5.21,9.45],height=0.2, color='blue')
+axs[2][2].barh(y_pos-0.1, [3.18,1.65,2.40],height=0.2, color='blue')
 axs[2][2].barh(y_pos-0.3, [1.70,2.71,1.51],height=0.2, color='green')
-axs[2][2].barh(y_pos+0.3, [35.63,35.62,35.71],height=0.2, color='cyan')
+axs[2][2].barh(y_pos+0.3, [26.74,36.69,25.10],height=0.2, color='cyan')
 
 axs[2][2].set_ylabel('Payload in bytes')
 axs[2][2].set_xlabel('Average latency (seconds)')
@@ -201,9 +201,9 @@ axs[2][2].invert_yaxis()
 
 axs[3][2].set_title('20 clients 1000tps')
 axs[3][2].barh(y_pos+0.1, [8.81,6.48,8.70],height=0.2, color="red")
-axs[3][2].barh(y_pos-0.1, [11.34,10.17,9.98],height=0.2, color='blue')
+axs[3][2].barh(y_pos-0.1, [7.04,6.71,6.46],height=0.2, color='blue')
 axs[3][2].barh(y_pos-0.3, [6.74,9.38,6.72],height=0.2, color='green')
-axs[3][2].barh(y_pos+0.3, [39.30,41.42,40.33],height=0.2, color='cyan')
+axs[3][2].barh(y_pos+0.3, [28.87,43.05,31.52],height=0.2, color='cyan')
 
 axs[3][2].set_ylabel('Payload in bytes')
 axs[3][2].set_xlabel('Average latency (seconds)')
