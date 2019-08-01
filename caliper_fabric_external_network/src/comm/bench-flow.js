aa385967ea30322b3b0e84dec6ea4c81d3f117d4
salmanah <salardahm@gmail.com>
2019-08-01 16:31:21 +0200

    Finished the remaining experiments and generated reports

--- caliper_fabric_external_network/src/comm/bench-flow.js
+++ caliper_fabric_external_network/src/comm/bench-flow.js
@@ -375,7 +375,7 @@ module.exports.run = async function(configFile, networkFile) {
         await monitor.stop();
 
         let date = new Date().toISOString().replace(/-/g,'').replace(/:/g,'').substr(0,15);
-        let output = path.join(process.cwd(), `reports/fabric-tls/500chars/500chars-20-clients.html`);
+        let output = path.join(process.cwd(), `reports/fabric-solo/500chars/500chars-20-clients.html`);
         await report.generate(output);
         logger.info(`Generated report at ${output}`);
 
